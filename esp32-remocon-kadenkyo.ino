615bc58f3bc9277eb12fcb0a43635d022d9b23fb
ikesato <ikesato76@gmail.com>
2017-08-23 05:13:04 +0900

    adjust best frequency and resolution

--- esp32-remocon-kadenkyo.ino
+++ esp32-remocon-kadenkyo.ino
@@ -2,7 +2,7 @@
 #define LEDC_CHANNEL_0     0
 
 // use resolution bit (precission) for LEDC timer
-#define LEDC_TIMER_RES_BIT  8
+#define LEDC_TIMER_RES_BIT  11
 
 // use 38 kHz as a LEDC base frequency
 #define LEDC_BASE_FREQ     38000
@@ -14,8 +14,8 @@
 // Arduino like analogWrite
 // value has to be between 0 and valueMax
 void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255) {
-  // calculate duty, 8191 from 2 ^ 13 - 1
-  uint32_t duty = (255 / valueMax) * min(value, valueMax);
+  // calculate duty, 2047 from 2 ^ 11 - 1
+  uint32_t duty = (2047 / valueMax) * min(value, valueMax);
 
   // write duty to LEDC
   ledcWrite(channel, duty);
