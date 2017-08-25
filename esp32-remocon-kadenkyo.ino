699e92d492fc0400f87dec0da91d81e30f4b722f
ikesato <ikesato76@gmail.com>
2017-08-26 05:07:21 +0900

    implement kadenkyo format

--- esp32-remocon-kadenkyo.ino
+++ esp32-remocon-kadenkyo.ino
@@ -40,62 +40,69 @@ void putLed(uint8_t v) {
 
 // Sharp のテレビの電源ボタン
 #define T 425 // [us]
-#define TRAILER_T 8000 // [us]
+#define TRAILER_T 69000 // [us]
 uint8_t leader[] = {8,4};
 uint8_t code[] = {0x55,0x5A,0xF1,0x48,0x68,0x8B};
 unsigned long prevTime = 0;
-int state = 0; // 0:idle 1:send LEADER 2:send DATA 3:wait trailer
+int state = 0; // 0:idle 1:send LEADER 2:send DATA 3:trailer hi 4:trailer lo
 int codePos = 0;
 int leaderPos = 0;
+uint8_t hilo;
 
 void loop() {
   unsigned long time, diff, t;
-  uint8_t hilo;
   uint8_t byte, bit;
+  time = micros();
+  diff = time - prevTime;
 
   if (state == 0) {
     if (digitalRead(BUTTON_PIN) == LOW) {
       state = 1;
       leaderPos = 0;
-      prevTime = micros();
       Serial.println("sending IR.");
+      prevTime = micros();
     }
   } else if (state == 1) {
     hilo = ((leaderPos & 0x01) == 0);
     putLed(hilo);
-    time = micros();
-    diff = time - prevTime;
     if (leader[leaderPos]*T <= diff) {
       leaderPos++;
       prevTime = time;
       if (leaderPos >= sizeof(leader)/sizeof(leader[0])) {
         state = 2;
         codePos = 0;
+        hilo = 1;
       }
     }
   } else if (state == 2) {
-    hilo = ((codePos & 0x01) == 0);
     putLed(hilo);
-    time = micros();
-    diff = time - prevTime;
     byte = code[codePos >> 3];
     bit = byte & (1 << (7 - (codePos & 0x7)));
-    if (hilo != 0 && bit != 0)
+    if (hilo == 0 && bit != 0)
       t = T * 3;
     else
       t = T;
-    if (T <= diff) {
-      codePos++;
+    if (t <= diff) {
+//printf("%2d %d 0x%02x 0x%02x %s\n", codePos, hilo, byte, bit, (t==T ? "T" : "3T"));
       prevTime = time;
-      if (codePos >= ((sizeof(code)/sizeof(code[0])) << 3)) {
+      if (hilo == 0)
+        codePos++;
+      hilo = !hilo;
+      if ((codePos >> 3) >= (sizeof(code)/sizeof(code[0]))) {
         state = 3;
       }
     }
   } else if (state == 3) {
+    putLed(1);
+    if (T <= diff) {
+      state = 4;
+      prevTime = time;
+    }
+  } else if (state == 4) {
     putLed(0);
-    diff = micros() - prevTime;
     if (TRAILER_T <= diff) {
       state = 0;
+      Serial.println("done.");
     }
   }
 }
